d97872cf10f7472a7a6cd79c52ad4d34a5006d7b
POde97 <69850262+POde97@users.noreply.github.com>
2023-03-27 18:46:38 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='Cell2CellMatch',
+    version='1.0',    
+    description='functional network of cells from scRna-seq ',
+    url='https://github.com/POde97/Cell2CellMatch',
+    author='Paolo Odello',
+    author_email='paoloodeo.o@gmail.com',
+    license='MIT license',
+    packages=['Cell2CellMatch'],
+    install_requires=['igraph==0.10.2',
+			'leidenalg',
+			'networkx==2.6.3'
+			]
+)
